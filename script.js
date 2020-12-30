f6a9e03acee7cd7fa5f1280371d2549cc5c6e881
Mutasim-Shatil <mutasimshatil@hotmail.com>
2020-12-30 11:59:56 +0800

    submit btn complete

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
+const generateDisplay = document.getElementById("generate-display");
+const pinDisplay = document.getElementById("pin-display");
+
 // generate random password
 const generateBtn = document.getElementById("generate-btn");
 generateBtn.addEventListener("click", function () {
   const generatePin = Math.round(Math.random() * (9999 - 1000 + 1)) + 1000;
-  const generateDisplay = document.getElementById("generate-display");
-//   const generateDisplayNum = parseInt(generateDisplay);
+  //   const generateDisplayNum = parseInt(generateDisplay);
   generateDisplay.value = generatePin;
 });
 const buttons = document.querySelectorAll(".button");
 for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function(){
-        let number = buttons[i].getAttribute("data-num");
-        const pinDisplay = document.getElementById("pin-display");
-        pinDisplay.value += number;
-    })
+  buttons[i].addEventListener("click", function () {
+    let number = buttons[i].getAttribute("data-num");
+    pinDisplay.value += number;
+  });
 }
+
+const submitBtn = document.getElementById("submit-btn");
+submitBtn.addEventListener("click", function () {
+    const pinWrong = document.getElementById("pin-wrong");
+    const pinMatch = document.getElementById("pin-match");
+    if (generateDisplay.value == pinDisplay.value){
+        pinMatch.style.display = "block";
+    }else{
+        pinWrong.style.display = "block";
+    }
+});
